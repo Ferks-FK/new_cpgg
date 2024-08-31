9e86281446f2f8ca8d9cd7236521be3ed4df4029
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2024-08-31 16:01:46 -0400

    Adjust gateways layout.

--- resources/views/modules/admin/gateways/index.blade.php
+++ resources/views/modules/admin/gateways/index.blade.php
@@ -14,9 +14,9 @@
                 <x-empty.message>No gateways have been added yet.</x-empty.message>
             </x-empty>
         @else
-            <div class="flex gap-5">
+            <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
                 @foreach ($gateways as $gateway)
-                    <div class="w-1/4 gap-5">
+                    <div class="w-full gap-5">
                         <div class="flex flex-col items-center justify-center p-2 rounded-md shadow-md bg-slate-600">
                             <img src="{{ $gateway->image }}" alt="{{ $gateway->name }}" class="mb-5 max-h-12"/>
                             <x-button as="link" href="{{ route('admin.gateways.edit', $gateway->type) }}" icon="icon.pencil" icon_size="!size-4">
